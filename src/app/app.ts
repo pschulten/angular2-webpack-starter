5360661713cb4c26a5a6572f6c57e033ed5fc104
pschulten <pschulten@dbschenker.com>
2016-02-25 16:09:04 +0100

    baby step 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -32,35 +32,7 @@ import {Home} from './home/home';
       background-color: lightgray;
     }
   `],
-  template: `
-    <header>
-      <nav>
-        <h1>Hello {{ name }}</h1>
-        <ul>
-          <li router-active>
-            <a [routerLink]=" ['Index'] ">Index</a>
-          </li>
-          <li router-active>
-            <a [routerLink]=" ['Home'] ">Home</a>
-          </li>
-          <li router-active>
-            <a [routerLink]=" ['About'] ">About</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <footer>
-      WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      <div>
-        <img [src]="angularclassLogo" width="10%">
-      </div>
-    </footer>
-  `
+  templateUrl: './app/app.html'
 })
 @RouteConfig([
   { path: '/', component: Home, name: 'Index' },
